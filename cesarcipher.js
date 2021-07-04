e38437d764db6ad13eea51eb59d4895fc3d001ed
TIJANI0826 <ibrahimtijani08@gmail.com>
2021-07-04 14:35:02 +0100

    Javascript freecodecamp

---
+++ cesarcipher.js
@@ -0,0 +1,40 @@
+// One of the simplest and most widely known ciphers is a Caesar cipher, also known as a shift cipher. In a shift cipher the meanings of the letters are shifted by some set amount.
+
+// A common modern use is the ROT13 cipher, where the values of the letters are shifted by 13 places. Thus A ↔ N, B ↔ O and so on.
+
+// Write a function which takes a ROT13 encoded string as input and returns a decoded string.
+
+// All letters will be uppercase. Do not transform any non-alphabetic character (i.e. spaces, punctuation), but do pass them on.
+
+// rot13("SERR PBQR PNZC") should decode to the string FREE CODE CAMP
+
+// rot13("SERR CVMMN!") should decode to the string FREE PIZZA!
+
+// rot13("SERR YBIR?") should decode to the string FREE LOVE?
+
+// rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.") should decode to the string THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.
+
+// 123456
+
+function rot13(str) {
+    list1 = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+    a = 'ssss'
+    a.
+    for (let index = 0; index < array.length; index++;) {
+        index_added = index + 13
+        if(index_added > array.length){
+            index_added = array.length - index 
+        }
+        
+        
+    }
+
+  return str;
+}
+
+rot13("SERR PBQR PNZC");
+
+/**
+* Your test output will go here
+*/
+/* Navigated to Learn JavaScript Algorithms and Data Structures Projects: Caesars Cipher | freeCodeCamp.org */
